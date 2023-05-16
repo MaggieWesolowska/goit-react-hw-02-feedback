5057477887cea3d6d0d9b8d168cbe973480d6b6d
MaggieWesolowska <maggiewes07@gmail.com>
2023-05-16 21:30:31 +0200

    edited app component to add class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,29 @@
+import React, { Component } from 'react';
 // import { Statistics } from './Statistics/Statistics';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {};
+
+  countPositiveFeedbackPercentage = () => {};
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      ></div>
+    );
+  }
+}
